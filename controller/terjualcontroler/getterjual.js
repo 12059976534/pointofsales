d3bfa9dab4b510a1159805748c17fad15922e908
wahyu120599 <andri.anugrah@bfi.co.id>
2022-07-30 17:29:12 +0700

    fix1

--- controller/terjualcontroler/getterjual.js
+++ controller/terjualcontroler/getterjual.js
@@ -9,7 +9,7 @@ controller.getterjualAll= async(req,res,next)=>{
     var limit = parseInt(req.query.limit)
         var offset = parseInt(req.query.offset)
       let get = await db.Terjual.findAndCountAll({
-        iclude:[barang],
+        iclude:['barang'],
         limit:limit,  
         offset:offset
       })
